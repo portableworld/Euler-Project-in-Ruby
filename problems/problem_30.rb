ba9526352a990cc5be16e401128caed3d5fbc70c
Charles Caldwell <charlie@unfalsedesign.com>
2012-09-12 22:07:11 -0500

    Formatting

--- problems/problem_30.rb
+++ problems/problem_30.rb
@@ -1,8 +1,8 @@
 sum = 0
 
 (2..10**6).each do |num|
-    puts "Checking #{num}"
-    nums = num.to_s.split(//).collect{|i| i.to_i}
+  puts "Checking #{num}"
+  nums = num.to_s.split(//).collect{|i| i.to_i}
 	power_sum = 0
 	nums.each {|i| power_sum += i**5}
 	sum += num if power_sum == num
