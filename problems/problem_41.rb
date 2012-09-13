11fb013bf47c1c794d363e22a2bd15e42a6b3ff3
Charles Caldwell <charlie@unfalsedesign.com>
2012-09-12 22:08:01 -0500

    Formatting

--- problems/problem_41.rb
+++ problems/problem_41.rb
@@ -3,9 +3,9 @@ require File.join(File.dirname(__FILE__), "..", "classes", "all_classes.rb")
 primes = Array.new
 
 Prime.each(10**8) do |n|
-    if n.is_pandigital?
-		primes << n
-		puts "Found #{n}"
+  if n.is_pandigital?
+    primes << n
+    puts "Found #{n}"
 	end
 end
 
