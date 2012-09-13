a63db94c933e1fb84e5c7fd5ca6b18a4d84b803f
Charles Caldwell <charlie@unfalsedesign.com>
2012-09-12 22:07:23 -0500

    Formatting and switch to require_relative

--- problems/problem_36.rb
+++ problems/problem_36.rb
@@ -1,10 +1,9 @@
-require File.join(File.dirname(__FILE__), "..", "classes", "all_classes.rb")
-
+require_relative '../classes/all_classes'
 sum = 0
 
 (1...10**6).each do |n|
-    puts "Checking #{n} --- #{sum}"
-    sum += n if n.is_palindrome?(10) && n.is_palindrome?(2)
+  puts "Checking #{n} --- #{sum}"
+  sum += n if n.is_palindrome?(10) && n.is_palindrome?(2)
 end
 
 puts "The result is: #{sum}"
