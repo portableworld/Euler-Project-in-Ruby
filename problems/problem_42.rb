3d408b1b0854816922253e2794b39394473ea35b
Charles Caldwell <charlie@unfalsedesign.com>
2012-09-12 22:08:53 -0500

    Cleaned up code especially some blocks.

--- problems/problem_42.rb
+++ problems/problem_42.rb
@@ -1,31 +1,20 @@
-require File.join(File.dirname(__FILE__), "..", "classes", "all_classes.rb")
-
-count = 0
-text = ""
-words_as_numbers = Array.new
-triangles = Array.new
+require_relative '../classes/all_classes'
 
 file = File.new(File.join(File.dirname(__FILE__), "..", "text_files", "words_for_42.txt"))
-file.each {|line| text = line}
+text = file.readline
+file.close
 
 text.gsub!("\"", "")
 words = text.split(",")
-words.each do |word|
+words_as_numbers = words.collect do |word|
   word_as_number = 0 # Could use a better name
   word.each_char do |c|
     word_as_number += c.to_s.to_place_in_alphabet
   end
-  words_as_numbers << word_as_number
+  word_as_number
 end
 
-(1..30).each do |n|
-  triangles << n.to_triangle
-end
+triangles = (1..30).collect(&:to_triangle)
+words_as_numbers.select! {|n| triangles.include? n}
 
-words_as_numbers.each do |num|
-  if triangles.include?(num)
-    count += 1
-  end
-end
-
-puts "The Solution is: #{count}"
+puts "The Solution is: #{words_as_numbers.count}"
