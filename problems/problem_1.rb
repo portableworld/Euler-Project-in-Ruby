fd7ca12935a5e0d4b1dba7351a0303c4d7537c70
Charles Caldwell <charlie@unfalsedesign.com>
2013-11-09 22:26:32 -0600

    Fixed range to be exclusive of 1000

--- problems/problem_1.rb
+++ problems/problem_1.rb
@@ -1,6 +1,6 @@
 require_relative '../classes/all_classes'
 
-sum = (1..1000).find_all do |num|
+sum = (1...1000).find_all do |num|
   num.is_multiple_of?(3) || num.is_multiple_of?(5)
 end.inject(:+)
 
