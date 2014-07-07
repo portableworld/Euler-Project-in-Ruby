a9ac1688fa832b4189c117896080a6590713dca3
Charles Caldwell <bookedge@gmail.com>
2014-07-07 11:04:39 -0500

    Replaced tab indentions with double spaces

--- classes/Bignum.rb
+++ classes/Bignum.rb
@@ -1,12 +1,10 @@
 class Bignum
-    def is_palindrome?(base = 10)
-        num = self
-		num = num.to_s(base)
-        (0...num.length).each do |i|
-            if num[i] != num[(num.length-1)-i]
-                return false
-            end
-        end
-		return true
-	end
+  def is_palindrome?(base = 10)
+    num = self
+    num = num.to_s(base)
+    (0...num.length).each do |i|
+      return false if num[i] != num[(num.length-1)-i]
+    end
+    true
+  end
 end
